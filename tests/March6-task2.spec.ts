4d750f927c9de6c14c2e050cf17b5b3d8a1ee4c3
bhaav <bhaav@gmail.com>
2026-03-07 00:06:19 +0530

    all task

--- tests/March6-task2.spec.ts
+++ tests/March6-task2.spec.ts
@@ -5,5 +5,5 @@ test("icc_ranking",async({page})=>{
     await page.locator('//button[@class="mx-2 w-1.5 h-1.5 bg-dark-background opacity-40 rounded-full transition-width delay-150 duration-500 ease-in-out"]').nth(2).click();
     let rating = await page.locator('(//section[@id="mens-batting-rankings"]/descendant::div[@class="swiper-slide max-w-[328px] lg:max-w-[450px] mr-3 lg:mr-0 last:mr-0 swiper-slide-next"]/descendant::span[@class="text-xs leading-140 font-extrabold uppercase text-blue-950"])[4]').textContent();
     console.log("ishan kishan ",rating)
-    await page.screenshot({path:'screenshot/task2.png'});
+    await page.screenshot({path:'March6task2.png'});
 })
