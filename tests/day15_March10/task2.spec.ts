76d7f27c1b65fed141805ba6b600bc3ea3ad7ada
bhaav <bhaav@gmail.com>
2026-03-10 22:40:15 +0530

    all task

---
+++ tests/day15_March10/task2.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@playwright/test";
+
+test("Gmail keyboard compose and send", async ({ page }) => {
+   // unable to login due to some google login security issue as Gmail blocks automated login
+
+
+    // await page.goto("https://mail.google.com/");
+    // await page.keyboard.press("Shift+c");
+    // await page.fill("textarea[name='to']", "recipient@example.com");
+    // await page.fill("input[name='subjectbox']", "Test Subject");
+    // await page.fill("div[aria-label='Message Body']", "Hello, this is a test email.");
+    // await page.keyboard.press("Control+Enter");
+    // await page.screenshot({ path: "gmail-compose.png" });
+    // await page.waitForTimeout(5000);
+});
